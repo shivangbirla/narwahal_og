e54e542b1dcb2155f6c8217548b00f6e6c2c9028
shivangbirla <birlashivang01@gmail.com>
2023-10-29 22:20:03 +0530

    signup page

--- src/App.js
+++ src/App.js
@@ -7,30 +7,32 @@ import Sidebar from "./componenets/Sidebar";
 import Base from "./componenets/Base";
 import { MainContext } from "./componenets/Context";
 import { Drawer } from "@mui/material";
+import Signup from "./componenets/Signup";
 
 const App = () => {
   const [searchValue, setSearchValue] = useState("areaa");
   const { open, setOpen } = useContext(MainContext);
   return (
-    <div className=" ">
-      <React.Fragment key={"left"}>
-        <Drawer anchor={"left"} open={open} onClose={() => setOpen(false)}>
-          <div className="w-[300px] pl-6">
-            <Sidebar />
-          </div>
-        </Drawer>
-      </React.Fragment>
-      {/* <Base /> */}
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Home searchValue={searchValue} setSearchValue={setSearchValue} />
-          }
-        />
-        <Route path="/zone" element={<Zone />} />
-      </Routes>
-    </div>
+    // <div className=" ">
+    //   <React.Fragment key={"left"}>
+    //     <Drawer anchor={"left"} open={open} onClose={() => setOpen(false)}>
+    //       <div className="w-[300px] pl-6">
+    //         <Sidebar />
+    //       </div>
+    //     </Drawer>
+    //   </React.Fragment>
+    //   {/* <Base /> */}
+    //   <Routes>
+    //     <Route
+    //       path="/"
+    //       element={
+    //         <Home searchValue={searchValue} setSearchValue={setSearchValue} />
+    //       }
+    //     />
+    //     <Route path="/zone" element={<Zone />} />
+    //   </Routes>
+    // </div>
+    <Signup />
   );
 };
 
